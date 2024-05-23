a129079651f3e9e9bb5946abe273cfd6fa61d2e2
Elias Muñoz <eliasjm28@gmail.com>
2024-05-22 20:12:14 -0500

    instalar express

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 'use strict';
-console.log("Works!")
+const express = require("express");
+
+const servidor = express();
+
+servidor.get("/", (solicitud, respuesta)=>{
+    respuesta.json({
+        saludo: "Hola"
+    })
+});
+
+servidor.get("/xxx", (solicitud, respuesta)=>{
+    respuesta.redirect(301, "/");
+})
+servidor.listen(2000)
